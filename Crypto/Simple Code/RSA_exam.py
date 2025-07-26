741a791e37ff05075b854dd479bd98c2a9bd3107
35-hasib <hrrahman35@gmail.com>
2025-07-27 00:45:50 +0600

    added RSA_exam.py

---
+++ Crypto/Simple Code/RSA_exam.py
@@ -0,0 +1,23 @@
+p = 37
+q = 79
+e = 19
+
+n = p * q
+phi = (p-1) * (q-1)
+
+d = pow(e,-1,phi)
+print(d)
+print(f"Public key (e, n): ({e}, {n})")
+
+c = int(input("Enter a number: "))
+m = pow(c,d,n)
+
+print(f"M: {m}")
+
+M = str(m)
+if(len(M) % 2 != 0): M = '0' + M
+
+result = ''
+for i in range(len(M)//2):
+    result += chr(ord('A')-1+(int(M[i]+M[i+1]) % 26))
+print("Decrypted message:", result)
